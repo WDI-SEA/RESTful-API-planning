147376b819b1fd3a04d4073cdede92ce3ef412e5
prijacash <prijacash@gmail.com>
2022-09-07 09:24:12 -0700

    updated with notes on deliverables js

---
+++ deliverable.js
@@ -0,0 +1,23 @@
+/*
+
+MEMBERS
+C - POST /members
+R - GET, /members/:member_id, GET, /members/new (creation form)
+U - PUT, /members/:member_id
+D - DELETE, /members/:member_id
+
+
+BOOKS
+C - POST, /books
+R - GET, /books (index), GET /books/:book_id (specific book), GET/ books/:member_id
+U - PUT, /books/:books_id, PUT books/:members_id
+D - DELETE, /books/:book_id
+
+BOOK GENRES
+C - POST, /genres
+R - GET, /genres, GET /genres/new (creation form)
+U - PUT, /genres/:genre_id
+D - DELETE /genres/:genre_id
+
+
+*/
